86c64740a9f0eaa12c9b8be9335f2c387d2a5e3b
midrissi01 <m.oulhaj_idrissi@mundiapolis.ma>
2024-01-03 19:40:56 +0100

    commit addPost

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import moment from "moment";
+import { HeartOutlined, CommentOutlined } from "@ant-design/icons";
 
-function Post({post}) {
+function Post({ post }) {
   return (
-    <div className="bs1 p-2">
-      <div className="d-flex">
-        <div className="d-flex">
+    <div className="bs1 p-2 mb-3">
+      <div className="d-flex justify-content-between align-items-center">
+        <div className="d-flex align-items-center ">
           {post.user.profilePicUrl == "" ? (
-            <span>{post.user.username[0]}</span>
+            <span className="ProfilPic1 d-flex align-items-center">
+              {post.user.username[0]}
+            </span>
           ) : (
             <span>{post.user.profilePicUrl}</span>
           )}
+          <Link className="ms-2">{post.user.username}</Link>
+        </div>
+        <div>
+          <p>{moment(post.createdAt).format("MMM DD yyyy")}</p>
+        </div>
+      </div>
+      <img src={post.image} className="postimage" />
+      <p className="mt-1 mb-1 text-start">{post.description}</p>
+      <div className="d-flex align-items-center">
+        <div className="d-flex align-items-center ms-3">
+          <HeartOutlined />
+          <p>{post.likes.length}</p>
+        </div>
+        <div className="d-flex align-items-center">
+          <CommentOutlined />
+          <p>{post.comments.length}</p>
         </div>
       </div>
     </div>
   );
 }
 
-export default Post
+export default Post;
